af3c01e3d1523f43c199fd116856cb86a9614184
petrero <petrero@gmail.com>
2020-07-13 11:42:15 +0300

    14.1. Typed Arguments &amp; Function Return Types(Section 3: Modern PHP 7 Introduction (OPTIONAL)
    In this lecture we will talk about the function and class method argument types and also the functions
    
    and methods return types.
    
    So let's you know function call it convert data and convert the function will accept one parameter called
    
    format but this parameter will be typed to base format.
    
    This means that this function expects the format parameter to be of type base format.
    
    And that means that the this should be any of the classes that actually extend that base for my abstract
    
    class.
    
    And that's because their base format is an abstract class.
    
    You cannot instantiate it.
    
    So in this case this would mean that it has to be a class that extends the base form up.
    
    So let's try to damp the result of calling convert data on their Dasent class.
    
    You see that it works both with Jason and with Exham L..

--- PHP/public/index.php
+++ PHP/public/index.php
@@ -5,49 +5,17 @@ require __DIR__.'/../vendor/autoload.php';
 use App\Format\JSON;
 use App\Format\XML;
 use App\Format\YAML;
-use App\Format\FromStringInterface;
-use App\Format\NamedFormatInterface;
+use App\Format\BaseFormat;
 
-print_r ("Interfaces<br />");
+print_r ("Typed arguments & return types<br /><br />");
 
 $data = [
     "name" => "John",
     "surname" => "Doe"
 ];
 
-$json = new JSON($data);
-$xml = new XML($data);
-$yml = new YAML($data);
-//$base = new BaseFormat($data);
-
-
-var_dump($json);
-echo "<br />";
-var_dump($xml);
-echo "<br />";
-var_dump($yml);
-echo "<br />";
-//var_dump($base);
-
-print_r("<br /><br />Result of conversion<br /><br />");
-
-$formats = [$json, $xml, $yml];
-foreach($formats as $format){
-  if($format instanceof NamedFormatInterface) {
-    var_dump($format->getName());
-    echo "<br />";
-  }
-  echo "<hr />";
-  echo 'Class name: ';
-  var_dump(get_class($format));
-  echo "<br />";
-  echo 'Convert result: ';
-  var_dump($format->convert());
-  echo "<br />";
-  var_dump($format instanceof FromStringInterface);
-  echo "<hr />";
-  if($format instanceof FromStringInterface) {
-    var_dump($format->convertFromString('{"name": "John", "surname": "Doe"}'));
-    echo "<br />";
-  }
+function convertData(BaseFormat $format){
+  return $format->convert();
 }
+
+var_dump(convertData(new JSON($data)));
