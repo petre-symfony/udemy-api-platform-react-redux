38d6ddd481d6385ad05a68b82d443a195e513d23
petrero <petrero@gmail.com>
2020-07-16 15:59:04 +0300

    56. Preparing JWT token library and keys(Section 8: API Platform - User Authentication and JWT tokens)
    Install jwt bundle: composer require lexik/jwt-authentication-bundle
    
    Generate the SSH keys:
     - mkdir -p config/jwt
     - openssl genpkey -out config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
     - openssl pkey -in config/jwt/private.pem -out config/jwt/public.pem -pubout
    
    Configure JWT token library

--- ApiPlatform/config/bundles.php
+++ ApiPlatform/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
