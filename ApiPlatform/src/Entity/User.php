b791bdfed7a27e06fa4f7eb8f9bb0992fbf13afe
petrero <petrero@gmail.com>
2020-07-16 15:08:45 +0300

    Second and third api resource

--- ApiPlatform/src/Entity/User.php
+++ ApiPlatform/src/Entity/User.php
@@ -7,9 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ApiResource()
  */
 class User implements UserInterface
 {
