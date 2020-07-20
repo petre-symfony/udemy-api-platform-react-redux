8b2fbc95d873c4b63a5176800992603121aa78d7
petrero <petrero@gmail.com>
2020-07-20 10:12:40 +0300

    101. API Resource for Image entity{set _api_receive}(Section 14: API Platform - Uploading Files (Images))
    There is a setting called _api_receive and set it to false.
    
    This will just forbit API platform of creating an instance of this class automatically for
    
    us from the request body.
    
    And this makes sense because we will be uploading an image and handling this work to this custom operation.

--- ApiPlatform/src/Entity/Image.php
+++ ApiPlatform/src/Entity/Image.php
@@ -8,7 +8,19 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 /**
  * @ORM\Entity()
  * @Vich\Uploadable()
- * @ApiResource()
+ * @ApiResource(
+ *   collectionOperations={
+ *     "get",
+ *     "post"={
+ *       "method"="POST",
+ *       "path"="/images",
+ *       "controller"=UploadImageAction::class,
+ *       "defaults" = {
+ *         "_api_receive"=false
+ *       }
+ *     }
+ *   }
+ * )
  */
 class Image {
   /**
